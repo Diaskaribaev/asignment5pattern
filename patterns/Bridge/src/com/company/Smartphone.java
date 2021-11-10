158e561c9d2277a786d2f0149a4e6b8016b352ca
Dias <51242971+Diaskaribaev@users.noreply.github.com>
2021-11-10 06:38:17 +0600

    Update Smartphone.java

--- patterns/Bridge/src/com/company/Smartphone.java
+++ patterns/Bridge/src/com/company/Smartphone.java
@@ -2,10 +2,10 @@ package com.company;
 
 public abstract class Smartphone {
 
-    //Composition - implementor
+    
     protected Os os;
 
-    //constructor with implementor as input argument
+    
     public Smartphone( Os c) {
         this.os = c;
     }
