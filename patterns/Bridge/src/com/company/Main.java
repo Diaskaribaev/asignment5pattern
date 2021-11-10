2bfde67d5fc0280e446ec80f8941346f9dcbf2d3
Dias <51242971+Diaskaribaev@users.noreply.github.com>
2021-11-10 06:36:43 +0600

    Update Main.java

--- patterns/Bridge/src/com/company/Main.java
+++ patterns/Bridge/src/com/company/Main.java
@@ -3,11 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//        Shape tri = new Triangle(new RedColor());
-//        Shape pent = new Pentagon(new GreenColor());
-//
-//        tri.applyColor();
-//        pent.applyColor();
 
         Smartphone samsung = new Samsung(new Android());
 
